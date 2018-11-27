9bb1d3d7e253eaef76736fcb7d5e177504dc3550
Damian <lo16wawa@stud.informatik.uni-erlangen.de>
2018-11-28 00:25:30 +0100

    created cmake and ran it

---
+++ CMakeFiles/TestApp.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cip/2014/lo16wawa/Starcraft/sc2-simulation/src/main.cpp" "/home/cip/2014/lo16wawa/Starcraft/sc2-simulation/CMakeFiles/TestApp.dir/src/main.cpp.o"
+  "/home/cip/2014/lo16wawa/Starcraft/sc2-simulation/src/simu/simulation.cpp" "/home/cip/2014/lo16wawa/Starcraft/sc2-simulation/CMakeFiles/TestApp.dir/src/simu/simulation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src/simu"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/simu/simulation.cpp
@@ -0,0 +1,7 @@
+#include "simulation.h"
+
+#include <iostream>
+
+void test() {
+	std::cout << "hello" << std::endl;
+}
